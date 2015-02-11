e62de78d64a4744308a8badc993799fc8be80489
KaviyaThangaraj <kavythangaraj@gmail.com>
2015-02-11 04:59:51 +0000

    First commit

---
+++ ofs.messaging-master/src/main/java/ofs/messaging/Mycallable.java
@@ -0,0 +1,42 @@
+package ofs.messaging;
+
+import java.util.concurrent.*;
+import java.util.*;
+ 
+class MyCallableTest implements Callable {
+ 
+   public String call() throws Exception {
+      Thread.sleep(2000);
+ 
+      // return name of thread.
+      return (Thread.currentThread().getName());
+   }
+}
+ 
+class MyCallable {
+   public static void main (String args[]) {
+ 
+      // thread pool size 5
+      ExecutorService executor = Executors.newFixedThreadPool(5);
+ 
+      // collection to store results
+      List<Future<String>> list = new ArrayList<Future<String>>();
+ 
+      // spawn 10 jobs
+      for(int i=0; i< 10; i++){
+         @SuppressWarnings("unchecked")
+		Future<String> future = executor.submit(new MyCallableTest()); 
+         list.add(future);
+      }
+ 
+      // print results
+      for(Future<String> future : list){ 
+         try {
+            System.out.println(future.get());
+         } catch (Exception e) {
+         }
+      }
+ 
+      executor.shutdown();
+   }
+}
---
+++ ofs.messaging-master/src/main/java/ofs/messaging/testEventCreation.java
@@ -0,0 +1,36 @@
+package ofs.messaging;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.ExecutionException;
+
+import javax.naming.NamingException;
+
+import org.apache.commons.configuration.ConfigurationException;
+
+import ofs.messaging.Models.Event;
+import ofs.messaging.Persistence.PersistenceManager;
+
+public class testEventCreation {
+
+  public testEventCreation() {
+    // TODO Auto-generated constructor stub
+  }
+
+  public static void main(String[] args) throws ConfigurationException, InterruptedException,
+      ExecutionException, KeyManagementException, NoSuchAlgorithmException, IOException, NamingException, URISyntaxException {
+    Event e = new Event("CREATION1");
+   // PersistenceManager.saveEvent(e);
+    e = new Event("PONR");
+    //PersistenceManager.saveEvent(e);
+
+    e = new Event("RELEASE");
+   // PersistenceManager.saveEvent(e);
+    e = new Event("DISPATCH");
+    //PersistenceManager.saveEvent(e);
+
+  }
+
+}
